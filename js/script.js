4c038b7c10e2fd1b1d87cf634b705f87ed7a95c1
carosp8 <carolinaperaltas8@gmail.com>
2024-05-24 01:08:05 -0300

    efecto sobre nombre, frase, profesion al cargar la pagina

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Agregar la clase 'fade-in' a los elementos después de que la página haya cargado
+    document.querySelector('.caro_nombre').classList.add('fade-in');
+    document.querySelector('.caro_frase').classList.add('fade-in');
+    document.querySelector('.caro_profesion').classList.add('fade-in');
+
+    document.querySelector('.isaac_nombre').classList.add('fade-in');
+    document.querySelector('.isaac_frase').classList.add('fade-in');
+    document.querySelector('.isaac_profesion').classList.add('fade-in');
+});
