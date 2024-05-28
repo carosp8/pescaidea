7e11c27ce278a805fcfdef225266fe8888827944
carosp8 <carolinaperaltas8@gmail.com>
2024-05-28 18:45:10 -0300

    Menu hamburguesa responsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Agregar Clase fade-in al nombre-frase-profesion //
+
 document.addEventListener("DOMContentLoaded", function() {
     // Agregar la clase 'fade-in' a los elementos después de que la página haya cargado
     document.querySelector('.caro_nombre').classList.add('fade-in');
@@ -8,3 +10,20 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('.isaac_frase').classList.add('fade-in');
     document.querySelector('.isaac_profesion').classList.add('fade-in');
 });
+
+// Agregar Clase visible al nav //
+
+const nav = document.getElementById('nav');
+const abrirNav = document.getElementById('abrir');
+const cerrarNav = document.getElementById('cerrar');
+const contenedorNav = document.querySelector('.contenedor-nav');
+
+abrirNav.addEventListener('click', () => {
+    nav.classList.add('visible');
+})
+
+cerrarNav.addEventListener('click', () => {
+    nav.classList.remove('visible');
+})
+
+
