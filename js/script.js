90c6ee041e7a1f212e7bab9fa5de0fbf99778971
carosp8 <carolinaperaltas8@gmail.com>
2024-05-29 15:49:31 -0300

    seccion proyectos responsive

--- js/script.js
+++ js/script.js
@@ -26,3 +26,17 @@ cerrarNav.addEventListener('click', () => {
 })
 
 
+// Proyecto Tarjetas //
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card =>{
+    card.addEventListener('click', () =>{
+        cards.forEach(c => c.classList.remove('active'));
+        card.classList.add('active')
+    })
+})
+
+
+
+
